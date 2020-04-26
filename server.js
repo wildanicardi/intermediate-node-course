67d128edc9296270604b131f83181a5f13c53c06
wildanicardi <wildanicardi9@gmail.com>
2020-04-26 22:05:15 +0700

    hash password and change to async await function

--- server.js
+++ server.js
@@ -1,73 +1,82 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 const port = 8000;
 const app = express();
 
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`server is listening on port:${port}`)
-})
-const User = require('./models/User');
-mongoose.connect('mongodb://localhost/userData', {
+  console.log(`server is listening on port:${port}`);
+});
+const User = require("./models/User");
+mongoose.connect("mongodb://localhost/userData", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
-})
+  useCreateIndex: true,
+});
 
 // response function
 function sendResponse(res, err, data) {
   if (err) {
     res.json({
       success: false,
-      message: err
-    })
+      message: err,
+    });
   } else if (!data) {
     res.json({
       success: false,
-      message: "Not Found"
-    })
+      message: "Not Found",
+    });
   } else {
     res.json({
       success: true,
-      data: data
-    })
+      data: data,
+    });
   }
 }
 // CREATE
-app.post('/users', (req, res) => {
-  User.create({
-      ...req.body.newData
+app.post("/users", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.newData.password, 10);
+
+  await User.create(
+    {
+      ...req.body.newData,
+      password: hashedPassword,
     },
     (err, data) => {
-      sendResponse(res, err, data)
-    });
-})
+      sendResponse(res, err, data);
+    }
+  );
+});
 
-app.route('/users/:id')
+app
+  .route("/users/:id")
   // READ
-  .get((req, res) => {
-    User.findById(req.params.id, (err, data) => {
-      sendResponse(res, err, data)
+  .get(async (req, res) => {
+    await User.findById(req.params.id, (err, data) => {
+      sendResponse(res, err, data);
     });
   })
   // UPDATE
-  .put((req, res) => {
-    User.findByIdAndUpdate(
-      req.params.id, {
-        ...req.body.newData
-      }, {
-        new: true
+  .put(async (req, res) => {
+    await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        ...req.body.newData,
+      },
+      {
+        new: true,
       },
       (err, data) => {
-        sendResponse(res, err, data)
-      })
+        sendResponse(res, err, data);
+      }
+    );
   })
   // DELETE
-  .delete((req, res) => {
-    User.findByIdAndDelete(req.params.id,
-      (err, data) => {
-        sendResponse(res, err, data)
-      })
-  })
+  .delete(async (req, res) => {
+    await User.findByIdAndDelete(req.params.id, (err, data) => {
+      sendResponse(res, err, data);
+    });
+  });
